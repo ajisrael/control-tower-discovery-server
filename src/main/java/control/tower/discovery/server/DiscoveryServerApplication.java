86a79fcade5b530fce0df25b25005b5754233c04
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-03 03:28:46 -0400

    a - discovery server project from spring initializer

---
+++ src/main/java/control/tower/discovery/server/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package control.tower.discovery.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
