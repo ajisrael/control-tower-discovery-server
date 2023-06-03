ef0ad1db787d3de11f7817aed1d9cdb8a94a2351
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-03 03:32:42 -0400

    f - configured eureka server

--- src/main/java/control/tower/discovery/server/DiscoveryServerApplication.java
+++ src/main/java/control/tower/discovery/server/DiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package control.tower.discovery.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
